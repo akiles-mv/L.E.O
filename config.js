ad23d52be13630ec000f6615b6e5dce2d975314a
Akiles-MV <vareloco58@gmail.com>
2026-06-17 17:23:06 -0600

    Agregar Configuracion incial del bot

---
+++ config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    restaurante: {
+        nombre: "QUIERO +",
+        telefono:"+5218134054104@c.mex"
+    },
+    grupo: {
+        nombre: "Grupo de oficina",
+    },
+
+    horarios:{
+        pedirMenu: "0 12 * * 1-5",   // 12:00 pm, Lunes - Viernes
+        cierreOrdenes: "30 13 * * 1-5", //13:30 pm, Lunes - Viernes
+        enviarOrden: "40 13 * * 1-5" //13:40 pm, Lunes - Viernes
+    },
+}
